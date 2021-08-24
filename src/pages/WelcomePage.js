a2978536e39e9beab4639821f1fbb10cc3150bb4
Alan Soto <alansfj348@gmail.com>
2021-08-24 18:14:17 -0500

    Welcome page text added and reducing padding.

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -10,6 +10,7 @@ const WelcomePage = () => {
         <h1 className="welcome-title">
           Welcome to "Nice ToDo", a free ToDo app
         </h1>
+        <p>"Nice ToDo" keeps your ToDos's even if you close your browser. 😉</p>
       </div>
       <div className="welcome-form-box">
         <WelcomeForm />
